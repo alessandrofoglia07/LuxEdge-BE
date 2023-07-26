46c365d711b3a020f46f640710cedbec44c3b761
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2023-07-26 17:42:03 +0200

    jwt util funtions added

---
+++ src/utils/auth.ts
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+import { IUserDocument } from '../types.js';
+
+/** Generate access token (expires in 15 minutes) */
+export const generateAccessToken = async (user: IUserDocument) => {
+    const token = jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN_SECRET as string, { expiresIn: '15m' });
+    return token;
+};
+
+/** Generate a refresh for a user and save it to the database (expires in 30 days) */
+export const generateRefreshToken = async (user: IUserDocument) => {
+    const refreshToken = jwt.sign({ userId: user._id }, process.env.REFRESH_TOKEN_SECRET as string, { expiresIn: '30 days' });
+    return refreshToken;
+};
