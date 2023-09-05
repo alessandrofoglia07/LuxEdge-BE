bdd4414e0cabca1dd6b8bcce8dfeb3dfe968b9fe
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2023-09-05 13:24:25 +0200

    Fixed product search

--- dist/models/product.js
+++ dist/models/product.js
@@ -1,12 +1,23 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import { Schema, model, SchemaTypes } from 'mongoose';
 const ProductSchema = new Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
     description: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
     price: {
         type: Number,
@@ -37,4 +48,21 @@ const ProductSchema = new Schema({
         default: []
     }
 }, { timestamps: true });
+ProductSchema.virtual('rating').get(function () {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (this.reviews.length === 0)
+            return 0;
+        yield this.populate('reviews');
+        const reviews = this.reviews;
+        const totalRating = reviews.reduce((acc, review) => {
+            const reviewDocument = review;
+            return acc + reviewDocument.rating;
+        }, 0);
+        const avgRating = totalRating / this.reviews.length;
+        return Math.round(avgRating * 10) / 10;
+    });
+});
+ProductSchema.set('toJSON', {
+    virtuals: true
+});
 export default model('Product', ProductSchema);
--- src/routers/products.ts
+++ src/routers/products.ts
@@ -31,32 +31,75 @@ router.get('/suggested', async (req: Request, res: Response) => {
 
 // get all products / search products with query
 router.get('/search', async (req: Request, res: Response) => {
-    const query = req.query.q ? String(req.query.q) : '';
     const tagsQuery = req.query.tags ? String(req.query.tags) : '';
+    const priceQuery = req.query.price ? String(req.query.price) : '';
+    const ratingQuery = req.query.rating ? String(req.query.rating) : '';
+    const sortQuery = req.query.sort ? String(req.query.sort) : '';
+
+    let sort: Record<string, number>;
+
+    switch (sortQuery) {
+        case 'price_asc':
+            sort = { price: 1 };
+            break;
+        case 'price_desc':
+            sort = { price: -1 };
+            break;
+        case 'recommend':
+            sort = { sold: -1 };
+            break;
+        case 'newest':
+            sort = { createdAt: -1 };
+            break;
+        case 'oldest':
+            sort = { createdAt: 1 };
+            break;
+        default:
+            sort = { sold: -1 };
+            break;
+    }
+
     const tags = tagsQuery.split(',').map((tag) => tag.trim());
+    const price = priceQuery.split(',').map((p) => Number(p.trim()));
+    const rating = ratingQuery.split(',').map((r) => Number(r.trim()));
 
     const limit = req.query.limit ? Number(req.query.limit) : 10;
     const page = req.query.page ? Number(req.query.page) : 1;
     const skip = (page - 1) * limit;
 
-    const products = await Product.aggregate([
-        {
-            $match: {
-                $and: [{ name: { $regex: query, $options: 'i' } }, { tags: { $in: tags } }]
-            }
-        },
-        { $limit: limit },
-        { $skip: skip },
-        { $lookup: { from: 'reviews', localField: '_id', foreignField: 'productId', as: 'reviews' } },
-        { $addFields: { rawScore: { $avg: '$reviews.rating' } } },
-        { $addFields: { score: { $round: ['$score', 2] } } },
-        { $unset: ['reviews', 'rawScore'] },
-        { $sort: { sold: -1 } }
-    ]);
+    const productsWithNoRatingFilter = await Product.where('tags')
+        .in(tags)
+        .where('price')
+        .gte(price[0] || 0)
+        .lte(price[1] || 999999)
+        .limit(limit)
+        .skip(skip)
+        .sort(sort as never);
 
-    if (products === undefined) return res.sendStatus(404);
+    const products = productsWithNoRatingFilter.filter((product) => {
+        if (!rating[0] || !rating[1]) return true;
+        const productRating = product.rating;
+        return productRating >= rating[0] && productRating <= rating[1];
+    });
 
-    res.json(products);
+    const totCountWithNoRatingFilter = await Product.where('tags')
+        .in(tags)
+        .where('price')
+        .gte(price[0] || 0)
+        .lte(price[1] || 999999);
+
+    const totCount = totCountWithNoRatingFilter.filter((product) => {
+        if (!rating[0] || !rating[1]) return true;
+        const productRating = product.rating;
+        return productRating >= rating[0] && productRating <= rating[1];
+    }).length;
+
+    if (products === undefined || productsWithNoRatingFilter === undefined) return res.sendStatus(404);
+
+    res.json({
+        products,
+        totCount
+    });
 });
 
 // get details of a product
